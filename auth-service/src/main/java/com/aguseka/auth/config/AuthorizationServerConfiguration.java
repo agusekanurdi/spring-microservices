79b46b8d54ed74aeb9d3a7ba264324ef47544533
Agus Ekanurdi <agus@ecquaria.com>
2018-08-27 12:42:03 +0800

    Remove Spring Cloud Gateway

--- auth-service/src/main/java/com/aguseka/auth/config/AuthorizationServerConfiguration.java
+++ auth-service/src/main/java/com/aguseka/auth/config/AuthorizationServerConfiguration.java
@@ -29,6 +29,6 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 .authorizedGrantTypes("authorization_code")
                 .scopes("user_info")
                 .autoApprove(true)
-                .redirectUris("http://localhost:8058/ui/login","http://localhost:8052/login");
+                .redirectUris("http://localhost:8058/ui/login","http://localhost:8052/login","http://localhost:8053/login");
     }
 }
